7d351b336a8144f4c9708372c7f108302b5f25d2
baskooijman <bas.kooijman@vu.nl>
2018-12-18 13:09:23 +0100

    Update check_entries.m

--- curation/check_entries.m
+++ curation/check_entries.m
@@ -3,7 +3,7 @@
 
 %%
 function [tree local server] = check_entries
-% created 2017/04/07 by Bas Kooijman
+% created 2017/04/07 by Bas Kooijman, modified 2018/12/18
 
 %% Syntax
 % [tree local server] = <../check_entries.m *check_entries*>
@@ -35,6 +35,24 @@ n_server = length(strfind(txt,'href="')); server = cell(n_server,1);
 for i = 1:n_server
   kill = strfind(txt,'href="'); txt(1:kill(1) + 5)= [];
   server{i} = txt(1:strfind(txt,'/"') - 1);
+end
+% cell string with server entries_web stored on server
+txt = urlread('https://www.bio.vu.nl/thb/deb/deblab/add_my_pet/entries_web/');
+head = strfind(txt,'folder.gif'); txt(1:head(1)) = []; 
+n_server_web = length(strfind(txt,'href="')); server_web = cell(n_server_web,1);
+for i = 1:n_server_web
+  kill = strfind(txt,'href="'); txt(1:kill(1) + 5)= [];
+  server_web{i} = txt(1:strfind(txt,'/"') - 1);
+end
+% cell string with server entries_zip stored on server
+txt = urlread('https://www.bio.vu.nl/thb/deb/deblab/add_my_pet/entries_zip/');
+n_zip = strfind(txt,'/icons/compressed.gif'); 
+n_server_zip = length(n_zip); server_zip = cell(n_server_zip,1); n_zip = [n_zip, length(txt) - 1];
+for i = 1:n_server_zip
+  txt_i = txt(n_zip(i):n_zip(i+1));
+  txt_0 = strfind(txt_i,'href="'); 
+  txt_1 = strfind(txt_i,'_2');
+  server_zip{i} = txt_i(txt_0+6:txt_1-1);
 end
 
 diff = setdiff(stat, local);
@@ -70,5 +88,29 @@ end
 diff = setdiff(server, local);
 if ~isempty(diff)
   fprintf('warning from check_entries: present in server, but not in local\n');
+  diff
+end
+
+diff = setdiff(server, server_web);
+if ~isempty(diff)
+  fprintf('warning from check_entries: present in entries on server, but not in entries_web on server\n');
+  diff
+end
+
+diff = setdiff(server_web, server);
+if ~isempty(diff)
+  fprintf('warning from check_entries: present in entries_web on server, but not in entries on server\n');
+  diff
+end
+
+diff = setdiff(server_zip, server_web);
+if ~isempty(diff)
+  fprintf('warning from check_entries: present in entries_zip on server, but not in entries_web on server\n');
+  diff
+end
+
+diff = setdiff(server_web, server_zip);
+if ~isempty(diff)
+  fprintf('warning from check_entries: present in entries_web on server, but not in entries_zip on server\n');
   diff
 end
