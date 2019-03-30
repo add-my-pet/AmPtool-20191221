f6341d0796ca91ed5576cafa1dbdb40e787e48df
baskooijman <bas.kooijman@vu.nl>
2019-03-30 17:24:07 +0100

    extension

--- curation/sort_get_link_eco.m
+++ curation/sort_get_link_eco.m
@@ -18,72 +18,48 @@ WD = pwd; % store current path
 path = which('get_link'); ind = strfind(path,'\');
 cd(path(1:ind(end)));
 
-entries = select; 
+entries = select; % species names in sequence that they have to be
 
-%% get_link
+sort_entries(entries, 'get_link.m')
+sort_entries(entries, 'get_eco.m')
 
-body = fileread('get_link.m');
-ind = strfind(body,' case '); ind = [ind, strfind(body,'    otherwise')]; n_ind = length(ind) - 1;
-head = body(1:ind(1)-1); tail = body(ind(end):end);
-for i = 1:n_ind
-  case_i = body(ind(i):ind(i+1)); nm_i = strfind(case_i,''''); nm_i = case_i(nm_i(1)+1:nm_i(2)-1);
-  txt.(nm_i) = case_i;
-end
-nm = fieldnames(txt);
-
-diff = setdiff(entries, nm);
-if ~isempty(diff)
-  fprintf('warning from sort_get_link_eco: present in select, but not in get_link\n');
-  diff
-  return
-end
-diff = setdiff(nm, entries);
-if ~isempty(diff)
-  fprintf('warning from sort_get_link_eco: present in get_link, but not in select\n');
-  diff
-  return
-end
-
-fid = fopen('get_link.m','w+');
-fprintf(fid, '%s', head);
-for i = 1:n_ind
-  fprintf(fid, '%s', txt.(entries{i}));
-end
-fprintf(fid, '%s', tail);
-fclose(fid);
-
-%% get_eco
-
-body = fileread('get_eco.m');
-ind = strfind(body,' case '); ind = [ind, strfind(body,'    otherwise')]; n_ind = length(ind) - 1;
-head = body(1:ind(1)-1); tail = body(ind(end):end);
-for i = 1:n_ind
-  case_i = body(ind(i):ind(i+1)); nm_i = strfind(case_i,''''); nm_i = case_i(nm_i(1)+1:nm_i(2)-1);
-  txt.(nm_i) = case_i;
-end
-nm = fieldnames(txt);
-
-diff = setdiff(entries, nm);
-if ~isempty(diff)
-  fprintf('warning from sort_get_link_eco: present in select, but not in get_eco\n');
-  diff
-  return
-end
-diff = setdiff(nm, entries);
-if ~isempty(diff)
-  fprintf('warning from sort_get_link_eco: present in get_eco, but not in select\n');
-  diff
-  return
-end
-
-fid = fopen('get_eco.m','w+');
-fprintf(fid, '%s', head);
-for i = 1:n_ind
-  fprintf(fid, '%s', txt.(entries{i}));
-end
-fprintf(fid, '%s', tail);
-fclose(fid);
-
-%% return to WD
-
+% return to WD
 cd(WD)
+
+end 
+function sort_entries(entries, fnNm)
+
+body = fileread(fnNm);
+
+% split the text of fnNm in head, tail and structure txt
+ind = [strfind(body,' case '), strfind(body,' otherwise')]; n_ind = length(ind) - 1;
+head = body(1:ind(1)-1); tail = body(ind(end):end);
+for i = 1:n_ind
+  case_i = body(ind(i):ind(i+1)-1); nm_i = strfind(case_i,''''); nm_i = case_i(nm_i(1)+1:nm_i(2)-1);
+  txt.(nm_i) = case_i;
+end
+nm = fieldnames(txt); % species-names as they occur in fnNm
+
+% check for differences in species names
+diff = setdiff(entries, nm);
+if ~isempty(diff)
+  fprintf(['Warning from sort_get_link_eco: present in select, but not in ', fnNm, '\n']);
+  diff
+  return
+end
+diff = setdiff(nm, entries);
+if ~isempty(diff)
+  fprintf(['Warning from sort_get_link_eco: present in ', fnNm, ', but not in select\n']);
+  diff
+  return
+end
+
+% write file
+fid = fopen(fnNm,'w+');
+fprintf(fid, '%s', head);
+for i = 1:n_ind
+  fprintf(fid, '%s', txt.(entries{i}));
+end
+fprintf(fid, '%s', tail);
+fclose(fid);
+end
